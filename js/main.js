b88a03b0c98a400194810360ada48ee6a2a57287
Chi Zeng <chi@chizeng.com>
2016-07-03 21:33:57 -0700

    Initial commit.

---
+++ js/main.js
@@ -0,0 +1,76 @@
+/**
+ * JS related to Chi's homepage.
+ */
+
+
+// Viewport width and height in pixels.
+var viewportWidth;
+var viewportHeight;
+
+var horizontalScroll;
+var verticalScroll;
+
+/**
+ * Size full-width images throughout the page.
+ */
+function sizeFullImages() {
+  // Leave 20px on either side of image. And 5px of border on either side.
+  var maxImageWidth = Math.min(700, viewportWidth - 50);
+  var fullImages = document.getElementsByClassName('fullImage');
+  for (var i = 0; i < fullImages.length; i++) {
+    var width = parseInt(fullImages[i].getAttribute('width'));
+    var height = parseInt(fullImages[i].getAttribute('height'));
+    var aspectRatio = width / height;
+    if (width > maxImageWidth) {
+      width = maxImageWidth;
+      fullImages[i].style.width = '' + width + 'px';
+      fullImages[i].style.height = '' + Math.round(width / aspectRatio) + 'px';
+    }
+  }
+}
+
+
+function setTopBackgroundHeight() {
+  document.getElementById('topBackground').style.height =
+      '' + viewportHeight + 'px';
+  // Push other content down appropriately.
+  document.getElementById('main').style.marginTop =
+      '' + (viewportHeight + 20) + 'px';
+}
+
+
+function makeTopBackgroundSemiTransparent() {
+  // Make the top bg image more transparent as user scrolls down.
+  var topBackgroundElement = document.getElementById('topBackground');
+  // Exponentially decay in opacity.
+  var opacity = Math.exp(
+      -Math.min(
+          1,
+          verticalScroll * 1.618 / topBackgroundElement.offsetHeight));
+  topBackgroundElement.style.opacity = opacity;
+}
+
+
+function handleResize() {
+  viewportWidth = document.documentElement.clientWidth;
+  viewportHeight = document.documentElement.clientHeight;
+  sizeFullImages();
+  setTopBackgroundHeight();
+}
+
+
+function handleScroll() {
+  horizontalScroll =
+      (window.pageXOffset || document.scrollLeft) - (document.clientLeft || 0);
+  verticalScroll =
+      (window.pageYOffset || document.scrollTop)  - (document.clientTop || 0);
+  makeTopBackgroundSemiTransparent();
+}
+
+
+// Initialize.
+handleResize();
+window.addEventListener('resize', handleResize, false);
+
+handleScroll();
+window.addEventListener('scroll', handleScroll, false);
